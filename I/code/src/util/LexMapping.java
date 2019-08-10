9455a1b23621c6b373d21a602abe0510ab338af1
Ryan Chew <me@ryan.moe>
2019-08-10 22:12:56 +1000

    documented lexmapping

--- I/code/src/util/LexMapping.java
+++ I/code/src/util/LexMapping.java
@@ -1,9 +1,16 @@
 package util;
 
+
+/**
+ * Contains relevant information for storage when paired with a word number in a hashmap
+ */
 public class LexMapping {
 
     //private String word;
+    /** The number of documents that contain this term */
     private int noDocuments;
+
+    /** The offset position to start reading in the inverted list file. */
     private Integer offset;
 
 
